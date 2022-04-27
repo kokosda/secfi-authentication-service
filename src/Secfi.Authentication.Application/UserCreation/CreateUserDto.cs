838feb4e53976e8d35e574cd5fd45d5a493021e9
Victor Ponamarev <vponamarev@guess.com>
2022-04-27 19:58:21 +0300

    CreateUserDto refactored.

--- src/Secfi.Authentication.Application/UserCreation/CreateUserDto.cs
+++ src/Secfi.Authentication.Application/UserCreation/CreateUserDto.cs
@@ -2,8 +2,5 @@
 
 public sealed class CreateUserDto
 {
-	public string Id { get; init; } = string.Empty;
-	public string Name { get; init; } = string.Empty;
-	public string Gtin { get; init; } = string.Empty;
-	public long TotalQuantity { get; init; }
+	public int HttpStatusCode { get; init; }
 }
