6b5565f337b4ee957beb652ba4b710b6e3f0dee8
Victor Ponamarev <vponamarev@guess.com>
2022-04-27 20:12:55 +0300

    UsersController added.

---
+++ src/Secfi.Authentication.Api/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Secfi.Authentication.Application.UserCreation;
+using Secfi.Authentication.Core.Handlers;
+
+namespace Secfi.Authentication.Api.Controllers;
+
+public sealed class UsersController : Controller
+{
+	private readonly IGenericCommandHandler<CreateUserCommand, CreateUserDto> _createUserCommandHandler;
+
+	public UsersController(IGenericCommandHandler<CreateUserCommand, CreateUserDto> createUserCommandHandler)
+	{
+		_createUserCommandHandler = createUserCommandHandler;
+	}
+
+	[HttpPost]
+	[Route("/")]
+	public async Task<IActionResult> CreateUser([FromBody] CreateUserCommand createUserCommand)
+	{
+		var createUserResponse = await _createUserCommandHandler.HandleAsync(createUserCommand);
+		return StatusCode(createUserResponse.Value.HttpStatusCode, createUserResponse.Messages);
+	}
+}
