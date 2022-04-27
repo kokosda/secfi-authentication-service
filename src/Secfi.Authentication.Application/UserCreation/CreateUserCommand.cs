82c61853665bfd6be092e04f3bccd478601839f1
Victor Ponamarev <vponamarev@guess.com>
2022-04-27 19:56:47 +0300

    null-reference types warnings eliminated.

--- src/Secfi.Authentication.Application/UserCreation/CreateUserCommand.cs
+++ src/Secfi.Authentication.Application/UserCreation/CreateUserCommand.cs
@@ -5,14 +5,14 @@ namespace Secfi.Authentication.Application.UserCreation;
 public sealed class CreateUserCommand
 {
 	[StringLength(128, MinimumLength = 3)]
-	public string Username { get; init; }
+	public string Username { get; init; } = string.Empty;
 
 	[StringLength(128, MinimumLength = 1)]
-	public string FirstName { get; init; }
+	public string FirstName { get; init; } = string.Empty;
 
 	[StringLength(128, MinimumLength = 1)]
-	public string LastName { get; init; }
+	public string LastName { get; init; } = string.Empty;
 
 	[StringLength(128, MinimumLength = 1)]
-	public string Password { get; init; }
+	public string Password { get; init; } = string.Empty;
 }
