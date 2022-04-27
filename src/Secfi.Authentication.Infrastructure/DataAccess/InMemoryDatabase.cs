9507d1f481d20134ff39d46d586e76a9180d8920
Victor Ponamarev <vponamarev@gmail.com>
2022-04-27 20:44:44 +0300

    InMemoryDatabase: storage replaced with ConcurrentDictionary to support multi-threading.

--- src/Secfi.Authentication.Infrastructure/DataAccess/InMemoryDatabase.cs
+++ src/Secfi.Authentication.Infrastructure/DataAccess/InMemoryDatabase.cs
@@ -1,18 +1,22 @@
-﻿namespace Secfi.Authentication.Infrastructure.DataAccess;
+﻿using System.Collections.Concurrent;
+
+namespace Secfi.Authentication.Infrastructure.DataAccess;
 
 public sealed class InMemoryDatabase : IInMemoryDatabase
 {
-	private readonly Dictionary<string,object> _storage;
+	private readonly ConcurrentDictionary<string,object> _storage;
 
 	public InMemoryDatabase()
 	{
-		_storage = new Dictionary<string, object>();
+		_storage = new ConcurrentDictionary<string, object>();
 	}
 
 	public void PersistObject<T>(T @object, string key) where T: class
 	{
 		var combinedKey = GetCombinedKey<T>(key);
-		_storage[combinedKey] = @object;
+
+		if (!_storage.TryAdd(combinedKey, @object))
+			throw new InvalidOperationException($"Cannot saved object with key {key} because it has already exists in the database.");
 	}
 
 	public T? GetPersistedObject<T>(string key) where T : class
